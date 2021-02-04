e144f9b0398e31bdfcf6731ed8161f42263b2116
Himanshu S <sleepybyte23@gmail.com>
2021-02-04 08:23:26 -0500

    update infura key

--- packages/react-app/src/constants.js
+++ packages/react-app/src/constants.js
@@ -1,10 +1,10 @@
 // MY INFURA_ID, SWAP IN YOURS FROM https://infura.io/dashboard/ethereum
-export const INFURA_ID = "7b7a5abb1551405cb514e818e779e9f9";
+export const INFURA_ID = "15cb60e90e1d414f9145ff62508a88c9";
+// 7b7a5abb1551405cb514e818e779e9f9
+// a369dd2063df43169ac8fe8909df6f0d
 
 //MY ETHERSCAN_ID, SWAP IN YOURS FROM https://etherscan.io/myapikey
 export const ETHERSCAN_KEY = "PSW8C433Q667DVEX5BCRMGNAH9FSGFZ7Q8";
-
-
 
 
 // EXTERNAL CONTRACTS
