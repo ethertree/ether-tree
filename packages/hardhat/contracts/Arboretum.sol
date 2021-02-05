e93b81b95b69178b1c8e32abe82409b9211c9103
rollodex <michael.glenn837@gmail.com>
2021-02-05 15:41:42 -0500

    New query function

--- packages/hardhat/contracts/Arboretum.sol
+++ packages/hardhat/contracts/Arboretum.sol
@@ -101,8 +101,7 @@ struct TreeInfo {
     uint fee; //Fee 
     uint startDate;        //Time before watering period starts (in seconds)
     uint waterersNeeded;   //Minimum players needed to join
-    uint planted; //Timestamp of when tree was planted
-    uint timesWatered; //Total number of payments made 
+    uint planted; //Timestamp of when tree was planted 
     
     address payable planter;       //Address of tree planter / owner
     //address[] waterers;    //Array of all tree waterers
@@ -219,7 +218,7 @@ contract Arboretum {
         
         Tree t = trees[id];
         
-        return 100 - ((t.finishedCount().mul(100)).div(t.numOfWaterers()));
+        return 100 - ((t.finishedCount().mul(100)).div(t.numOfWateres()));
     }
     
     //Returns how much the Planter would earn in fees if lapse limit was hit:
@@ -266,9 +265,29 @@ contract Arboretum {
         t.fundsRaised = tree.fundsRaised();
         t.finishedCount = tree.finishedCount();
         t.planted = tree.planted();
-        t.timesWatered = tree.timesWatered();        
-
+        
        return t; 
+    }
+    
+    function treesJoined() public view returns (uint[] memory) {
+        
+        uint i = 0; 
+        uint c = 0;
+        uint[] memory treeIds = new uint[](1024);
+        
+        while (i < treeCount) {
+            
+            (uint fE, uint nD, uint lD) = statsForTree(i,msg.sender);
+            
+            if (nD > 0) {
+                treeIds[c] = i;
+                c++;
+            }
+            
+            i++;
+        }
+        
+        return treeIds;
     }
     
     event JoinTree(uint _id, address _waterer);
@@ -291,7 +310,6 @@ contract Tree is CoreType {
     uint public startDate;        //Time before watering period starts (in seconds)
     uint public waterersNeeded;   //Minimum players needed to join
     uint public planted;          //What time the tree was planted
-    uint public timesWatered;     //Total payments made
     
     address payable public planter;       //Address of tree planter / owner
     address[] public waterers;    //Array of all tree waterers
@@ -368,8 +386,7 @@ contract Tree is CoreType {
             statsForTree[user].fruitEarned += 1;
             statsForTree[user].lastDue = stats.nextDue;
             statsForTree[user].nextDue = stats.nextDue.add(treeDuration.div(paymentFrequency));
-            timesWatered++;            
-
+            
             if (statsForTree[user].fruitEarned == paymentFrequency) { //--NEW: Keeps track of count of all players who met their payments 
                finishedCount++; 
                leftToClaim++;
@@ -441,8 +458,8 @@ contract Tree is CoreType {
         
     }
     
-    //Return waterers.length (as it is needed in parent contract)
-    function numOfWaterers() public view returns (uint) {
+    //--NEW: Return waterers.length (as it is needed in parent contract)
+    function numOfWateres() public view returns (uint) {
         return waterers.length;
     }
 }
