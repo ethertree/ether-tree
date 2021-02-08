41266d8d4b642cc191c17c9a10c24fdcee0f3a02
Adil Hussain <adilh621@gmail.com>
2021-02-07 20:31:35 -0500

    static pages generated

--- packages/react-app/src/views/About.jsx
+++ packages/react-app/src/views/About.jsx
@@ -1,13 +1,111 @@
 /* eslint-disable jsx-a11y/accessible-emoji */
 
 import React from "react";
+import "./About.css"
+import sampleImg from "../img/plant.png"
+import castle from "../teamPfp/castle.jpg"
+import sleepy from "../teamPfp/sleepy.jpg"
+import ziggy from "../teamPfp/ziggy.jpg"
+import adil from "../teamPfp/adil.jpg"
+import mj from "../teamPfp/mj.png"
 
 
 export default function About() {
 
+  const sectionStyle = {
+    textAlign: "left"
+  }
+  const textStyle = {
+    textAlign: "left",
+    fontSize:"16px"
+  }
+
   return (
     <div>
-      <p>About Us</p>
+      {/*
+        ⚙️ Here is an example UI that displays and sets the purpose in your smart contract:
+      */}
+      <div style={{ padding: 16, width: 1000, margin: "auto", marginTop: 64 }}>
+        {/* <h2>{treeCount} - Trees - </h2> */}
+
+        
+
+        <div>
+          <h1 style={sectionStyle}>
+            What is Ether Tree?
+          </h1>
+          <p style={textStyle}>
+          EtherTree is a decentralised application built on Ethereum. 
+          It is a stake game where players are exposed to Lapse Risk. 
+          The gamification is around planting seeds, watering trees and then harvesting their fruit.
+          </p>
+        </div>
+        <div style={{paddingTop : "10px"}}>
+          <h1 style={sectionStyle}>
+            Why Should I Play Ether Tree?
+          </h1>
+          <p style={textStyle}>
+          Investors can lower their total risk by diversifying across different assets. The less correlation, 
+          the greater the diversification benefit. When markets fall, traditional assets see their correlation with each other increase. 
+          This means the diversification benefit is reduced when it is needed most. EtherTree introduces investors to Lapse Risk which 
+          has a negative correlation to traditional assets. This means it can provide a portfolio with a real diversification benefit and reduce total risk.
+          </p>
+        </div>
+
+    <div className="container">
+    <h1 className="heading">Meet Our Team</h1>
+
+    <div className="profiles">
+      <div className="profile">
+        <img src={mj} class="profile-img" />
+
+        <h3 className="user-name">Michael Jordan</h3>
+        
+        <p>Michael is a creative actuary who has been thinking about blockchain since 2014.</p>
+      </div>
+      <div className="profile">
+        <img src={ziggy} style={{}}  className="profile-img" />
+
+        <h3 className="user-name">Zakiah Ray</h3>
+        
+        <p>Zakiah is a designer with a background in FinTech and a new-found passion for crypto and NFTs.</p>
+      </div>
+      <div className="profile">
+        <img src={castle}  className="profile-img" />
+
+        <h3 className="user-name">Michael Colton</h3>
+        
+        <p>Michael discovered Bitcoin in 2011 and started mining and trading shortly thereafter. His enthusiasm of the protocol and for software development lead him to discover Ethereum, 
+          and he began Solidity development for decentralized apps in 2017, 
+          including contributions to some of the first NFT platforms, such as Curio cards. His other hobbies include photography and retro gaming.</p>
+      </div>
     </div>
+
+
+    <div className="profiles">
+      <div className="profile">
+        <img src={adil}  className="profile-img" />
+
+        <h3 className="user-name">Adil Hussain</h3>
+        
+        <p>Adil is a full Stack JavaScript developer and a second year computer science student who’s interested in learning more about blockchain.</p>
+      </div>
+      <div className="profile">
+        <img src={sleepy}  className="profile-img" />
+
+        <h3 className="user-name">Himanshu Sethi</h3>
+        
+        <p>developer</p>
+      </div>
+    </div>
+  </div>
+
+
+        
+
+
+      </div>
+    </div>
+
   );
 }
--- packages/react-app/src/views/Faq.jsx
+++ packages/react-app/src/views/Faq.jsx
@@ -3,22 +3,30 @@
 import React from "react";
 import Faq from "react-faq-component";
 
+const textContentTitle = {
+  textAlign : "left"
+}
+const textContent = {
+  textAlign : "left",
+  marginLeft: "40px"
+}
+
 const data = {
   title: "What do I need to play?",
   rows: [
     {
       title: <h1>A computer running Chrome or Firefox?</h1>,
       content: (
-        <div>
+        <div style={textContentTitle}>
           Can I use a different browser?
           <p>
             We recommend sticking with Chrome or Firefox. It may be technically possible to use another browser, but we
             can’t guarantee optimal performance, so you may want to steer clear.
           </p>
-          <div>
+          <div style={textContentTitle}>
             Can I log in from multiple computers?
             <p>
-              You can use CryptoKitties from multiple computers as long as you have your digital wallet installed on
+              You can use EtherTree from multiple computers as long as you have your digital wallet installed on
               both.
             </p>
           </div>
@@ -30,14 +38,14 @@ const data = {
       content: (
         <div>
           <p>
-            <h1>Installing your digital wallet</h1>
-            <ul>
+            <h1 style={textContentTitle}>Installing your digital wallet</h1>
+            <ul style={textContent}>
               <li>
-                To use CryptoKitties, you need a digital wallet. We support all digital wallets, but recommend{" "}
-                <a href="https://www.meetdapper.com/?utm_source=twitter&amp;utm_medium=organicsocial&amp;utm_campaign=meetdapper">
-                  Dapper
+                To use EtherTree, you need a digital wallet. We support{" "}
+                <a href="https://docs.metamask.io/guide/">
+                  Metamask
                 </a>{" "}
-                for the best CryptoKitties experience.{" "}
+                for the best Ether Tree experience.{" "}
               </li>
               <li>
                 You’ll need to put money in your wallet to make your first purchase. We can show you how to do that,
@@ -48,9 +56,9 @@ const data = {
                 your password or lose your recovery kit.
               </li>
             </ul>
-            <h1>What is a “wallet address”?</h1>
-            <p>
-              Your public wallet address (e.g. 0xaba935f589805095a892ecefdb6eb83eff45d67) is a unique identifier for
+            <h1 style={textContentTitle}>What is a “wallet address”?</h1>
+            <p style={textContentTitle}>
+              Your public wallet address (e.g. 0xaba933f589805095a892ecefdb6eb73eff44d67) is a unique identifier for
               your wallet. It’s like a name. You can share it freely with others, and it’s used to direct assets to your
               wallet.{" "}
             </p>
@@ -63,21 +71,64 @@ const data = {
       content: (
         <div class="Collapse-body">
           <p>
-            <h2 id="whats-ether-eth-why-do-i-need-it">What’s ether (ETH)? Why do I need it?</h2>
-            <ul>
+            <h1 style={textContentTitle}>What’s ether (ETH)? Why do I need it?</h1>
+            <ul style={textContentTitle}>
               <li>
-                Ether is a digital currency that powers the Ethereum network, which is what CryptoKitties is built on.
+                Ether is a digital currency that powers the Ethereum network, which is what EtherTree is built on.
                 Ether acts like any other currency — its value fluctuates with the market.
               </li>
               <li>
                 You need to convert your currency (e.g. USD, CAD, GBP) into ether to pay for things — such as
-                CryptoKitties — on the Ethereum network.
+                EtherTree — on the Ethereum network.
               </li>
             </ul>
           </p>
         </div>
       ),
     },
+  ],
+};
+
+const data2 = {
+  title: "Ethics Of EtherTree",
+  rows: [
+    {
+      title: <h1>EtherTree is a wealth redistribution game</h1>,
+      content: (
+        <div>
+          <p>
+            <h1 style={textContentTitle}>Is it alright to win at the expense of someone else?</h1>
+            <ul style={textContent}>
+              <li>
+              The majority of us would agree that it is wrong to steal from others or trick them into a bad deal.
+              </li>
+              <li>
+              The majority of us would agree that if the rules are transparent and that if entering the game is entirely optional, then there is no ethical dilemma.
+              </li>
+            </ul>
+
+          </p>
+        </div>
+      ),
+    },
+    {
+      title: <h1 style={textContentTitle}>EtherTree is built on the ethereum blockchain and so all parameters and logic are transparent.</h1>,
+      content: (
+        <div class="Collapse-body">
+          <p>
+            <h1 style={textContentTitle}>No one is forced to play EtherTree.</h1>
+            <p style={textContentTitle}>
+            So yes it might not be nice to win at someone else’s expense but since they 
+            chose to enter the game and had perfect information about it beforehand, 
+            it is extremely difficult to make the case that it is unethical.
+            </p>
+          </p>
+
+        </div>
+      ),
+    }
+      
+   
   ],
 };
 const styles = {
@@ -90,9 +141,13 @@ const styles = {
 
 export default function Faqs() {
   return (
-    <div style={{ display: "flex", justifyContent: "center" }}>
-      <div className="container" style={{ width: "1000px", margin: "0 auto" }}>
+    <div style={{ display: "grid", gridTemplate: "1fr / 1fr" ,placeItems:"center", padding: 16, width: 1000, margin: "auto", marginTop: 64 }}>
+      <div  style={{ width: "850px", margin: "0 auto",zIndex:"1",paddingBottom : "50px" }}>
         <Faq data={data} style={styles} />
+      </div>
+      
+      <div style={{ width: "850px", margin: "0 auto",zIndex:"2" }}>
+        <Faq data={data2} style={styles} />
       </div>
     </div>
   );
